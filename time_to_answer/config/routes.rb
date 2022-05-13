e56f178ffae138246a13126987e642877d7b50ab
Everson <eversonpereiraofc@gmail.com>
2022-05-13 11:16:20 -0300

    initialize new project, login with deviser and using i18n

---
+++ time_to_answer/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :users
+  devise_for :admins
+  get 'welcome/index'
+  get 'inicio', to: 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "welcome#index"
+end
