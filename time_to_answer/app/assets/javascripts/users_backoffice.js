015e03bbbe1b75384c9f1e4916ad5c5451692462
Everson <eversonpereiraofc@gmail.com>
2022-05-17 11:01:49 -0300

    add layout to admins and users

---
+++ time_to_answer/app/assets/javascripts/users_backoffice.js
@@ -0,0 +1,6 @@
+//= require jquery/dist/jquery.min.js
+//= require bootstrap/dist/js/bootstrap.bundle.min.js
+//= require fastclick/lib/fastclick.js
+//= require nprogress/nprogress.js
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js
+//= require custom.min
--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :users_backoffice do
     get 'welcome/index'
   end
@@ -8,9 +11,9 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
   get 'welcome/index'
-  get 'inicio', to: 'welcome#index'
+  get 'inicio', to: 'site/welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "welcome#index"
+  root to: "site/welcome#index"
 end
